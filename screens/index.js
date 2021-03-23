3ef9adeac0ca3d708c7c7679a8bd4482b0a267ca
swapnil-ahlawat <swapnil.ahlawat@gmail.com>
2021-03-23 23:17:54 +0530

    add initial app

---
+++ screens/index.js
@@ -0,0 +1,11 @@
+import Home from "./Home"
+import Restaurant from "./Restaurant"
+import OrderDelivery from "./OrderDelivery"
+import SignUp from "./SignUp"
+
+export {
+    Home,
+    Restaurant,
+    OrderDelivery,
+    SignUp
+}
