767a42770524ff0511f59dcee9b32ef5187e5006
Piyush Maheshwari <maheshwaripiyush@live.in>
2021-03-28 15:49:39 +0530

    error handling bug removed

--- Backend/controllers/login-controllers.js
+++ Backend/controllers/login-controllers.js
@@ -4,12 +4,14 @@ const User = require('../database/User');
 const login = async(req, res, next) => {
     const {phoneNo, password, userType } = req.body;
 
-    const identifiedUser = await User.find({phoneNo, password, userType}).exec();
+    const identifiedUser = await User.find({phoneNo, password, userType}).exec().catch((error) => {
+        console.log("Error catched.", error);
+    });
     console.log(identifiedUser);
     if(identifiedUser.length===0){
         const error = Error('Invalid credentials.');
         error.code = 401;
-        throw error;
+        return next(error);
     }
     res.json({
         message: 'Logged in successfully!',
